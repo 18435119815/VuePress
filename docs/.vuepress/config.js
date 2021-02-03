d20a349cf51fbff9fbd817ce833901f6eaa34e70
junf.ren <junf.ren@sunyard.com>
2021-02-03 09:08:54 +0800

    'vuepress项目初始化'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Mr哎呦喂的博客',
+    description: '欢迎来到我的小窝',
+    themeConfig: {
+      logo: '/assets/img/hero.png',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/about' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+    }
+  }
